d8bd490616289043829b1b685eca307151b0e18c
Misrraim <misrraimsp@gmail.com>
2020-09-13 17:15:14 +0100

    -added unit tests for AccountServer (save and findAll) -added web integration tests for AccountController

--- src/test/java/misrraimsp/technest_rest_api/controller/AccountControllerTest.java
+++ src/test/java/misrraimsp/technest_rest_api/controller/AccountControllerTest.java
@@ -1,9 +1,11 @@
 package misrraimsp.technest_rest_api.controller;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import misrraimsp.technest_rest_api.model.Account;
 import misrraimsp.technest_rest_api.service.AccountServer;
 import misrraimsp.technest_rest_api.util.AccountModelAssembler;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,11 +36,39 @@ class AccountControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockBean
     private AccountModelAssembler accountModelAssembler;
 
     @MockBean
     private AccountServer accountServer;
+
+    private static Account account;
+    private static EntityModel<Account> accountEntityModel;
+
+    @BeforeAll
+    static void initializeData(){
+        account = new Account();
+        accountEntityModel = EntityModel.of(account);
+    }
+
+    private void setAccount(Long id, String name, Currency currency, BigDecimal balance, boolean treasury) {
+        account.setId(id);
+        account.setName(name);
+        account.setCurrency(currency);
+        account.setBalance(balance);
+        account.setTreasury(treasury);
+    }
+
+    private void setAccountEntityModel() {
+        accountEntityModel = EntityModel.of(
+                account,
+                linkTo(methodOn(AccountController.class).oneAccount(account.getId())).withSelfRel(),
+                linkTo(methodOn(AccountController.class).allAccounts()).withRel("accounts")
+        );
+    }
 
     @Test
     public void allAccounts_whenValidMethodAndUrlAndPathVariable_thenReturns200() throws Exception {
@@ -76,21 +106,79 @@ class AccountControllerTest {
     void oneAccount_whenValidInput_thenReturnsRequestedAccountResource() throws Exception {
         // given
         Long accountId = 1L;
-        Account account = new Account();
-        account.setId(accountId);
-        account.setName("testAccount");
-        account.setCurrency(Currency.getInstance("EUR"));
-        account.setBalance(BigDecimal.ONE);
-        account.setTreasury(false);
-        EntityModel<Account> accountEntityModel = EntityModel.of(
-                account,
-                linkTo(methodOn(AccountController.class).oneAccount(account.getId())).withSelfRel(),
-                linkTo(methodOn(AccountController.class).allAccounts()).withRel("accounts")
-        );
+        setAccount(accountId,"testingAccount",Currency.getInstance("EUR"),BigDecimal.ONE,false);
+        setAccountEntityModel();
         given(accountServer.findById(any(Long.class))).willReturn(account);
         given(accountModelAssembler.toModel(account)).willReturn(accountEntityModel);
         // when
         ResultActions resultActions = mockMvc.perform(get("/accounts/{accountId}", accountId));
+        // then
+        resultActions.andExpect(jsonPath("id").value(accountEntityModel.getContent().getId()));
+        resultActions.andExpect(jsonPath("name").value(accountEntityModel.getContent().getName()));
+        resultActions.andExpect(jsonPath("currency").value(accountEntityModel.getContent().getCurrency().toString()));
+        resultActions.andExpect(jsonPath("balance").value(accountEntityModel.getContent().getBalance()));
+        resultActions.andExpect(jsonPath("treasury").value(accountEntityModel.getContent().isTreasury()));
+        resultActions.andExpect(jsonPath("_links.self.href").value(accountEntityModel.getRequiredLink(IanaLinkRelations.SELF).toUri().toString()));
+        resultActions.andExpect(jsonPath("_links.accounts.href").value(accountEntityModel.getRequiredLink("accounts").toUri().toString()));
+    }
+
+    @Test
+    public void newAccount_whenValidMethodAndUrlAndRequestBody_thenReturns201() throws Exception {
+        // given
+        Long accountId = 1L;
+        setAccount(accountId,"testAccount",Currency.getInstance("EUR"),BigDecimal.ONE,false);
+        setAccountEntityModel();
+        given(accountServer.save(account)).willReturn(account);
+        given(accountModelAssembler.toModel(account)).willReturn(accountEntityModel);
+        // when
+        ResultActions resultActions = mockMvc.perform(post("/accounts")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(account))
+        );
+        // then
+        resultActions.andExpect(status().isCreated());
+    }
+
+    @Test
+    public void newAccount_whenInvalidRequestBody_thenReturns400() throws Exception {
+        // given
+        // ...
+        // when
+        ResultActions resultActions = mockMvc.perform(post("/accounts"));
+        // then
+        resultActions.andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void newAccount_whenValidInput_thenCallsAccountServerSave() throws Exception {
+        // given
+        Long accountId = 1L;
+        setAccount(accountId,"testAccount",Currency.getInstance("EUR"),BigDecimal.ONE,false);
+        setAccountEntityModel();
+        given(accountServer.save(account)).willReturn(account);
+        given(accountModelAssembler.toModel(account)).willReturn(accountEntityModel);
+        // when
+        mockMvc.perform(post("/accounts")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(account)));
+        // then
+        ArgumentCaptor<Account> accountArgumentCaptor = ArgumentCaptor.forClass(Account.class);
+        verify(accountServer, times(1)).save(accountArgumentCaptor.capture());
+        assertThat(accountArgumentCaptor.getValue()).isEqualTo(account);
+    }
+
+    @Test
+    void newAccount_whenValidInput_thenReturnsTheSavedAccount() throws Exception {
+        // given
+        Long accountId = 1L;
+        setAccount(accountId,"testAccount",Currency.getInstance("EUR"),BigDecimal.ONE,false);
+        setAccountEntityModel();
+        given(accountServer.save(account)).willReturn(account);
+        given(accountModelAssembler.toModel(account)).willReturn(accountEntityModel);
+        // when
+        ResultActions resultActions = mockMvc.perform(post("/accounts")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(account)));
         // then
         resultActions.andExpect(jsonPath("id").value(accountEntityModel.getContent().getId()));
         resultActions.andExpect(jsonPath("name").value(accountEntityModel.getContent().getName()));
--- src/test/java/misrraimsp/technest_rest_api/service/AccountServerTest.java
+++ src/test/java/misrraimsp/technest_rest_api/service/AccountServerTest.java
@@ -11,7 +11,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Currency;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,21 +28,27 @@ class AccountServerTest {
     @Mock
     private AccountRepository accountRepository;
 
+    private static Account account;
+
     @BeforeEach
     public void initializeData(){
         accountServer = new AccountServer(accountRepository);
+        account = new Account();
+    }
+
+    private void setAccount(Long id, String name, Currency currency, BigDecimal balance, boolean treasury) {
+        account.setId(id);
+        account.setName(name);
+        account.setCurrency(currency);
+        account.setBalance(balance);
+        account.setTreasury(treasury);
     }
 
     @Test
     public void findById_whenExists_returnsTheAccount() {
         // given
         Long accountId = 1L;
-        Account account = new Account();
-        account.setId(accountId);
-        account.setName("testAccount");
-        account.setCurrency(Currency.getInstance("EUR"));
-        account.setBalance(BigDecimal.ONE);
-        account.setTreasury(false);
+        setAccount(accountId,"testAccount",Currency.getInstance("EUR"),BigDecimal.ONE,false);
         given(accountRepository.findById(accountId)).willReturn(Optional.of(account));
         // when
         Account actualAccount = accountServer.findById(accountId);
@@ -61,6 +69,29 @@ class AccountServerTest {
         catch (EntityNotFoundByIdException e) {
             assertThat(e.getMessage()).isEqualTo("Entity of class " + e.getClassName() + " not found by id=" + accountId);
         }
+    }
+
+    @Test
+    public void save_whenValidInput_returnsTheAccount() {
+        // given
+        Long accountId = 1L;
+        setAccount(accountId,"anotherTestAccount",Currency.getInstance("USD"),BigDecimal.ZERO,true);
+        given(accountRepository.save(account)).willReturn(account);
+        // when
+        Account actualAccount = accountServer.save(account);
+        // then
+        assertThat(actualAccount).isEqualTo(account);
+    }
+
+    @Test
+    public void findAll_whenCalled_returnsAccountRepositoryOutput() {
+        // given
+        List<Account> accounts = new ArrayList<>();
+        given(accountRepository.findAll()).willReturn(accounts);
+        // when
+        List<Account> actualAccounts = accountServer.findAll();
+        // then
+        assertThat(actualAccounts).isEqualTo(accounts);
     }
 
 }
